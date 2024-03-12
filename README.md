fc2506292c4e564aa6d49ec2e1981176f87ede48
leandramusto@gmail.com <leandramusto@gmail.com>
2024-03-12 17:33:00 +0100

    add, div class button with event biding + @click,add methods; function to activate imgs when thumbnail is clicked, next prev function, autoscroll for img loop,set interval of 3 sec.

--- README.md
+++ README.md
@@ -16,4 +16,16 @@ regola d'oro: riciclare ovunque possibile!:
 
 - Questo significa che per la parte di markup possiamo recuperare html e css dell'esercizio svolto qualche giorno fa: è già tutto pronto!
   il riciclo spesso va a braccetto con le funzioni! Sapendole sfruttare bene, l'esercizio si riduce a poche righe ;)
-  Buon divertimento e confermate lettura come al solito :baby-yoda:
+
+# tools
+
+# Data:
+
+- array with 5 images that contains the paths
+- The activeImage variable represents the index of the currently displayed image. I've set it to 0 initially and then chang in 1
+
+# Methods:
+
+- nextPrev(isNext): This method is responsible for navigating to the next or previous image based on the isNext parameter. It also includes logic to loop through the images when reaching the beginning or end of the array.
+- changeImage(index): This method is triggered when a thumbnail is clicked, changing the active image to the one corresponding to the clicked thumbnail.
+- autoScroll(): This method uses setInterval to automatically scroll to the next image every 3 seconds
--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      // Array of image paths
       images: [
         "./assets/img/01.jpg",
         "./assets/img/02.jpg",
@@ -10,8 +11,34 @@ createApp({
         "./assets/img/04.jpg",
         "./assets/img/05.jpg",
       ],
-      activeImage: 0,
-      autoPlayInterval: null,
+      activeImage: 1,
     };
+  },
+  methods: {
+    // Function to navigate to the next or previous image
+    nextPrev(isNext) {
+      if (isNext) this.activeImage++;
+      else this.activeImage--;
+
+      // Check to loop through images
+      if (this.activeImage === this.images.length) {
+        this.activeImage = 0;
+      } else if (this.activeImage < 0) {
+        this.activeImage = this.images.length - 1;
+      }
+    },
+    // Function to change the active image when a thumbnail is clicked
+    changeImage(index) {
+      this.activeImage = index;
+    },
+    // Automatic image loop
+    autoScroll() {
+      setInterval(() => {
+        this.nextPrev(true); // Set to true for automatic scrolling to the next image
+      }, 3000); // 3secs
+    },
+  },
+  mounted() {
+    this.autoScroll(); //put a autoscroll when the component is mounted
   },
 }).mount("#app");
