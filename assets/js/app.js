60292e29bc6d74a6a33027c9d67125d4fd991da1
leandramusto@gmail.com <leandramusto@gmail.com>
2024-03-18 13:09:23 +0100

    add caracteristics to the pisctures;owner and caratheristics

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
+      ownerImages: "Leandra Musto, student of Boolean",
       images: [
         "./assets/img/01.jpg",
         "./assets/img/02.jpg",
         "./assets/img/03.jpg",
         "./assets/img/04.jpg",
         "./assets/img/05.jpg",
+      ],
+      imageCharacteristics: [
+        "These are the goats of Capri",
+        "These are the Faraglioni of Capri, the rocks",
+        "These are the Faraglioni of Capri, the rocks",
+        "These are the Faraglioni of Capri, the rocks",
+        "These are the via Krupp of Capri, walking-road",
       ],
       activeImage: 0,
     };
